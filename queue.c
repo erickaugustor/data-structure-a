0724e773cef2ad4604346383eb743cf6135fd9ec
Erick - <erick.augustoramos@gmail.com>
2017-12-05 18:17:13 -0200

    + queue

---
+++ queue.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Structs
+typedef struct tno{
+	int chave;
+	struct tno *prox;
+} no;
+
+//Protótipos
+int insere_inicio(int N, no **inicio, no **fim);
+
+
+void main(void){
+	no *inicio = NULL;
+	no *fim = NULL;
+	
+	int opcao, N, retorno;
+
+	do{
+		printf("  Menu de opcoes:\n\n");
+		printf("  1.Inserir um no!\n\n\n");
+		
+		printf(" Digite uma opcao: ");
+			scanf("%i", &opcao);
+			
+			switch(opcao){
+				case 1:
+					printf("Digite um valor para adicionar: ");
+					scanf("%i", &N);
+
+					retorno = (insere_inicio(N, &inicio, &fim));
+
+					if(retorno == 1) printf("O registro: %d foi adicionado com sucesso no endereco %d! \n\n", N, (* inicio));
+					else printf("Erro!");
+
+					break;
+			}
+
+			printf("Digite algo para prosseguir: \n");
+	        scanf("%s", &resp);
+
+            system("cls");
+
+		}while(opcao != 0);
+}
+
+
+int insere_inicio(int N, no **inicio, no **fim){
+	no *novo = (no *) malloc(sizeof(no));
+	if(novo == NULL) return(-1);
+
+	novo -> chave = N;
+	novo -> prox = (* inicio);
+	
+	if((*inicio)==NULL){
+		(*inicio) = (*fim) = novo;
+	}else{
+		(*fim) -> prox = novo;
+		(*fim) = novo;
+	}
+
+	return(1);
+}
